7f7baa6e04b5ab834827614bb48208e8539f41b9
ture9200 <didtjddms09@naver.com>
2022-06-03 18:03:32 +0900

    additionals

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -27,11 +27,11 @@ function LandingPage() {
     const fetchMovies = (endpoint) => { /* 로드되자마자 endpoint 이용한다. */ 
         fetch(endpoint)
             .then(response => response.json()) /* response 를 json 으로 해서 가져오는데 */ 
-            .then(response => { /* 그 결과값을 다시 response로 가져오면 20개가 보이는데 이름, overview, poster image등을 보여주게 된다. */ 
+            .then(response => { /* 그 결과값을 다시 response로 가져오면 20개 가장 유명한 영화가 보이는데 이름, overview, poster image등을 보여주게 된다. */ 
                 console.log(response)
                 setMovies([...Movies, ...response.results])  /* response 에 가져온것들을 movie state 에 넣기 위해서 set movies 한다음 result 넣어주면 movie state에 들어가 있다. */ 
-                /* ... response.results 하면 처음 20개는 사라지고 다음 20개가 덮어진다. 덮어씌우는게 아니라 있던거에 추가를 하려면 movie state 를 넣어주면 된다. */ 
-                setMainMovieImage(response.results[0]) 
+                /* ... response.results만 하면 처음 20개는 사라지고 다음 20개가 덮어진다. 덮어씌우는게 아니라 원래 있던거에 추가를 하려면 movie의 state 를 넣어주면 된다. */ 
+                setMainMovieImage(response.results[0]) /* response에 있는 첫번째 result를 setMainMovieImage에 넣어준다 */ 
                 setCurrentPage(response.page) /* 현재 페이지 정보를 넣어준다 */ 
             })
     }
@@ -40,7 +40,7 @@ function LandingPage() {
     /* 버튼을 누를때마다 다음 20개가 로딩 될 수 있도록한다 */ 
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage + 1}`;
         /* page 2 부터 시작인데 2 이후에도 load more 버튼을 눌러야하니까 그떼는 페이지가 2,3,4 이렇게 되서 current page라는 state 를 만들어주는데
-        currentpage  가 1일때는 2가 되고 2일때는 3 이 된다 */ 
+        currentpage  가 처음엔 0이니까 0일때는 1, 1일때는 2가 되고 2일때는 3 이 된다 */ 
         fetchMovies(endpoint)
 
     }
@@ -53,7 +53,8 @@ function LandingPage() {
             {MainMovieImage &&
                 <MainImage
 
-                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}  /* Main image 가 있으면 이렇게 하라 */ 
+                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}  /* Main image 가 있으면 이렇게 하라
+                    backdrop_path 는 img url 이다  */ 
                     title={MainMovieImage.original_title} /* 이미지의 title 을 가져오기 위해서 title 부여 */ 
                     text={MainMovieImage.overview} /* 이미지의 텍스트를 가져오기 위해서 text 부여 */ 
                 />
@@ -69,13 +70,14 @@ function LandingPage() {
 
                 <Row gutter={[16, 16]} > 
                     
-                    {Movies && Movies.map((movie, index) => (  /* movies가 있으면 메서드를 이용해서 하나하나 컨트롤 할 수 있게 하나의  무비들을 가져온다 
+                    {Movies && Movies.map((movie, index) => (  /* movies가 있으면 map메서드를 이용해서 하나하나 컨트롤 할 수 있게 하나의  무비들을 가져온다 
                     리액트의 fragment 로 감싸준다. 키값을 넣어줘야 에러가 안난다.  Row gutter 주면 가로 세로 여백 설정된다.*/ 
                         <React.Fragment key={index}> 
                             <GridCards      /* prop으로 grid card 의 정보들을 뿌려줘야한다. prop을 넣어준다. */  
                                 landingPage
                                 image={movie.poster_path ?
                                     `${IMAGE_BASE_URL}w500${movie.poster_path}` : null}  /* 이미지 정보를 movie 에 넣어준다 사이즈를 500으로 준다. 
+                                    poster path 가 있는 영화면 image base url 을 넣어준다.
                                     posterpath 가 없는것도 처리해줘야한다. 없으면 null 로처리  */ 
                                 movieId={movie.id} /* 고유 영화정보가 들어가야 하기 때문에 아이디를 줘야한다. */ 
                                 movieName={movie.original_title} /* 영화의 네임정보 제공 */ 
